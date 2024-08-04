9a50ec99ef72cedd3d1b23c337e106c4dd888888
葉奕緯 <aaa24295234@gmail.com>
2024-08-04 20:18:27 +0800

    first commit

---
+++ _nuxt/static/1722773876/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fworkinbar\u002F_nuxt\u002Fstatic\u002F1722773876",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fworkinbar\u002F",assetsPath:"\u002Fworkinbar\u002F_nuxt\u002F",cdnURL:a}}}}(null));
