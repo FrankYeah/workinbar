d0ae993fbb10983515776894808a5327279566d0
葉奕緯 <aaa24295234@gmail.com>
2024-08-04 22:04:55 +0800

    first commit

---
+++ _nuxt/static/1722780238/frontend-class/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fworkinbar\u002F_nuxt\u002Fstatic\u002F1722780238",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffrontend-class",config:{_app:{basePath:"\u002Fworkinbar\u002F",assetsPath:"\u002Fworkinbar\u002F_nuxt\u002F",cdnURL:a}}}}(null));
